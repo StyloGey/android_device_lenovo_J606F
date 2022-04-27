d5c3253ed4e41bb9325f014f7568035e70bf1a40
StyloGey <stylogey@gmail.com>
2022-04-27 16:25:20 +0200

    unsaged stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,8 @@ BOARD_KERNEL_PAGESIZE := 4096
 KERNEL_SD_LLVM_SUPPORT := true
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware/awinic
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += msm_poweroff.download_mode=0
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Partitions

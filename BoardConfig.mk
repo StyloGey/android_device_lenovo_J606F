7426baba6a816ee8e1cdef13e246a34a878feb0f
StyloGey <stylogey@gmail.com>
2022-04-27 16:25:12 +0200

    stuff v1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,4 +6,128 @@
 
 DEVICE_PATH := device/lenovo/J606F
 
-$(call inherit-product-if-exists, vendor/lenovo/BoardConfigVendor.mk)
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    product \
+    recovery \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+
+# Audio
+
+
+# AVB
+BOARD_AVB_ENABLE := true
+
+BOARD_AVB_VBMETA_SYSTEM := system
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := bengal
+
+# Build rules
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Directories
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Display
+TARGET_SCREEN_DENSITY := 240
+
+# DLKM
+BOARD_DO_NOT_STRIP_VENDOR_MODULES := true
+
+# DRM
+TARGET_ENABLE_MEDIADRM_64 := true
+
+# DTB
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+# Kernel
+TARGET_KERNEL_VERSION := 4.19
+KERNEL_DEFCONFIG := j606f_defconfig
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+KERNEL_SD_LLVM_SUPPORT := true
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware/awinic
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 100663296
+BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+BOARD_SUPER_PARTITION_SIZE := 12884901888
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 110004875264
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_SUPER_PARTITION_GROUPS := lenovo_dynamic_partitions
+BOARD_LENOVO_DYNAMIC_PARTITIONS_SIZE := 6438256640
+BOARD_LENOVO_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
+
+BOARD_USES_METADATA_PARTITION := true
+
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/android_touch/gesture_control"
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/recovery.fstab
+BOARD_INCLUDE_RECOVERY_DTBO := true
+
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
+# Sensors
+USE_SENSOR_MULTI_HAL := true
+
+# VINTF
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
+
+# Wlan
+BOARD_HAS_QCOM_WLAN := true
+WIFI_DRIVER_BUILT := qca_cld3
+WIFI_DRIVER_DEFAULT := qca_cld3
+WIFI_DRIVER_INSTALL_TO_KERNEL_OUT := true
+-include device/qcom/wlan/bengal/BoardConfigWlan.mk
+
+-include vendor/lenovo/J606F/BoardConfigVendor.mk

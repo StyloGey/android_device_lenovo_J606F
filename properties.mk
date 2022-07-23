8f359c83d97a40092024beb5404d468f5f711a91
Tobias Merkel <stylogey@gmail.com>
2022-07-23 18:35:52 +0200

    j606f: switch to fbe v2

--- properties.mk
+++ properties.mk
@@ -4,8 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Crypto
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.crypto.volume.filenames_mode=aes-256-cts
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.hardware.keystore_desede=true
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.crypto.volume.metadata.method=dm-default-key \
+    ro.crypto.volume.options=::v2 \
+    ro.hardware.keystore_desede=true

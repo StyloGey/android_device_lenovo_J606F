12d4d5f3381cacfbf1d6f145899fa5d5021240c8
Tobias Merkel <stylogey@gmail.com>
2022-07-27 20:28:05 +0200

    j606f: use telephony component

--- device.mk
+++ device.mk
@@ -181,6 +181,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     media-legacy \
     overlay \
     perf \
+    telephony \
     usb \
     vibrator \
     wlan
@@ -197,6 +198,14 @@ PRODUCT_PACKAGES += \
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
+
+# RIL
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.3.vendor:64 \
+    android.hardware.radio.deprecated@1.0.vendor:64 \
+    android.hardware.radio@1.6.vendor:64 \
+    android.hardware.secure_element@1.2 \
+    android.hardware.secure_element@1.2.vendor:64
 
 # Update engine
 PRODUCT_PACKAGES += \
